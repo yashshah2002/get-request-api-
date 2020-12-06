614f07ad5113fbd0a767da63fc0bfd1266328111
yash shah <68466768+yashshah2002@users.noreply.github.com>
2020-12-06 12:11:02 +0530

    Update post requests.py

--- post requests.py
+++ post requests.py
@@ -1,4 +1,4 @@
 import requests
-in_values = {'username':'Jack','password':'Hello'}
+in_values = {'username':'yash','password':'yash'}
 res = requests.post('https://httpbin.org/post',data = in_values)
 print(res.text)
